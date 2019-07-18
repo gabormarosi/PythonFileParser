50bf9cd3f881d4f144ef91c147f9bd9113876c8d
Gábor Marosi <Gabor.Marosi@wup.hu>
2019-07-18 17:53:11 +0200

    deployment template oc config dockerfile

--- PythonFileParser/FileParserBatch.py
+++ PythonFileParser/FileParserBatch.py
@@ -1,11 +1,17 @@
 import pika
 import datetime
+import os
 
 try:
-    filepath = '/Users/gmarosi/main/trx.csv'
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    filepath = os.environ['PARSER_PATH']
+    credentials = pika.PlainCredentials(os.environ['PARSER_USERNAME'],
+                                        os.environ['PARSER_PASSWORD'])
+    connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ['PARSER_HOSTNAME'],
+                                                                   os.environ['PARSER_PORT'],
+                                                                   '/',
+                                                                   credentials))
     channel = connection.channel()
-    channel.queue_declare(queue='rtd.dwh.transaction')
+    channel.queue_declare(queue=os.environ['PARSER_QUEUE_NAME'])
     with open(filepath) as fp:
         line = fp.readline()
         print("Header:"+line.strip())
@@ -13,7 +19,7 @@ try:
         line = fp.readline()
         while line:
             channel.basic_publish(exchange='',
-                                  routing_key='rtd.dwh.transaction',
+                                  routing_key=os.environ['PARSER_QUEUE_NAME'],
                                   body=line.strip())
             line = fp.readline()
 finally:
